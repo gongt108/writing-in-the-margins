c5ce205d13472b2cad15b7fe84bdf2de98c77885
Tiffany Gong <gongt108@gmail.com>
2024-02-26 09:53:09 -0800

    complete amazonscraper logic

--- bookscraper/bookscraper/spiders/amazon_search_product.py
+++ bookscraper/bookscraper/spiders/amazon_search_product.py
@@ -4,11 +4,33 @@ import urllib.parse
 
 # from urllib.parse import urljoin
 import re
-from base.models import Notification
+import psycopg2
 
 # from bookscraper.bookscraper.items import AmazonItem
 
 from bookscraper.items import AmazonItem
+
+
+# def get_notifications():
+#     connection = psycopg2.connect(
+#         host="localhost", user="tiffanygong", password="testpass", dbname="project4"
+#     )
+
+#     cur = connection.cursor()
+#     # Execute the query
+#     cur.execute(
+#         "SELECT title, contributors, price, email FROM base_book JOIN base_notification ON base_book.id = base_notification.book_id"
+#     )
+
+#     # Fetch all rows from the result set
+#     notifications = cur.fetchall()
+
+#     # Close the cursor and the connection
+#     cur.close()
+#     connection.close()
+
+#     # Return the fetched notifications
+#     return notifications
 
 
 class AmazonSearchProductSpider(scrapy.Spider):
@@ -19,71 +41,86 @@ class AmazonSearchProductSpider(scrapy.Spider):
             "data/%(name)s_%(time)s.csv": {
                 "format": "csv",
             }
-        }
+        },
+        "ITEM_PIPELINES": {
+            "bookscraper.pipelines.AmazonscraperPipeline": 400,
+        },
     }
 
-    def start_requests(self):
-        notifications = Notification.objects.all()
-        if len(notifications) == 0:
-            return
-        # keyword_list = ["ipad"]
-        for notification in notifications:
-            keyword = (
-                f"{notification.book.title} by {notification.book.contributors[0]}"
-            )
-            parsed_keyword = urllib.parse.quote_plus(keyword)
-            amazon_search_url = f"https://www.amazon.com/s?k={parsed_keyword}&page=1"
-            # TODO yield one search result
-            yield scrapy.Request(
-                url=amazon_search_url,
-                callback=self.discover_product_urls,
-                meta={
-                    "keyword": parsed_keyword,
-                    "target_price": notification.price,
-                    "email": notification.email,
-                },
-            )
 
-        # keyword_list = ["ipad"]
-        # for keyword in keyword_list:
-        #     amazon_search_url = f"https://www.amazon.com/s?k={keyword}&page=1"
-        #     yield scrapy.Request(
-        #         url=amazon_search_url,
-        #         callback=self.discover_product_urls,
-        #         meta={"keyword": keyword, "page": 1},
-        #     )
+def start_requests(self):
+    print("starting spider")
+    # Call the function and print the notifications
+    notifications = [
+        (
+            "Harry Potter and the Sorcerer’s Stone",
+            "J.K. Rowling, Olly Moss",
+            29,
+            "tiffanygong@yahoo.com",
+        )
+    ]
+    # notifications = get_notifications()
+    # print("notifications", notifications)
 
-    def discover_product_urls(self, response):
-        keyword = response.meta["page"]
-        target_price = response.meta["target_price"]
-        email = response.meta["email"]
+    if len(notifications) == 0:
+        return
 
-        ## Discover Product URLs
-        search_products = response.css(
-            "div.s-result-item[data-component-type=s-search-result]"
-        )[1]
-        for product in search_products:
-            # grabbing portion of url unique to the product
-            relative_url = product.css("h2>a::attr(href)").get()
-            product_url = urllib.parse.urljoin(
-                "https://www.amazon.com/", relative_url
-            ).split("?")[0]
-            yield scrapy.Request(
-                url=product_url,
-                callback=self.parse_product_data,
-                meta={"keyword": keyword, "target_price": target_price, "email": email},
-            )
+    for notification in notifications:
+        title = notification[0]
+        contributors = notification[1].split(",")
+        price = notification[2]
+        email = notification[3]
+        keyword = f"{title} by {contributors[0]}"
+        parsed_keyword = urllib.parse.quote_plus(keyword)
 
-    def parse_product_data(self, response):
-        amazon_item = AmazonItem()
-        price = response.css('.a-price-whole span[aria-hidden="true"] ::text').get("")
-        target_price = response.meta["target_price"]
+        amazon_search_url = f"https://www.amazon.com/s?k={parsed_keyword}"
 
-        if not price:
-            price = response.css(".a-price .a-offscreen ::text").get("")
+        # TODO yield one search result
+        # yield scrapy.Request(
+        #     url=amazon_search_url,
+        #     callback=self.discover_product_urls,
+        #     meta={
+        #         "keyword": parsed_keyword,
+        #         "target_price": notification.price,
+        #         "email": notification.email,
+        #     },
+        # )
 
-        if price is not None and price < target_price:
-            amazon_item["name"] = response.css("#productTitle::text").get("").xstrip()
-            amazon_item["price"] = price
-            amazon_item["email"] = response.meta["email"]
-            yield amazon_item
+
+def discover_product_urls(self, response):
+    keyword = response.meta["keyword"]
+
+    ## Discover Product URLs
+    search_products = response.css(
+        "div.s-result-item[data-component-type=s-search-result]"
+    )[1]
+    for product in search_products:
+        # grabbing portion of url unique to the product
+        relative_url = product.css("h2>a::attr(href)").get()
+        product_url = urllib.parse.urljoin(
+            "https://www.amazon.com/", relative_url
+        ).split("?")[0]
+        yield scrapy.Request(
+            url=product_url,
+            callback=self.parse_product_data,
+            meta={
+                "keyword": keyword,
+                "target_price": response.meta["target_price"],
+                "email": response.meta["email"],
+            },
+        )
+
+
+def parse_product_data(self, response):
+    amazon_item = AmazonItem()
+    price = response.css('.a-price-whole span[aria-hidden="true"] ::text').get("")
+    target_price = response.meta["target_price"]
+
+    if not price:
+        price = response.css(".a-price .a-offscreen ::text").get("")
+
+    if price is not None and price < target_price:
+        amazon_item["name"] = response.css("#productTitle::text").get("").xstrip()
+        amazon_item["price"] = price
+        amazon_item["email"] = response.meta["email"]
+        yield amazon_item
--- bookscraper/bookscraper/spiders/pricespider.py
+++ bookscraper/bookscraper/spiders/pricespider.py
@@ -1,9 +1,18 @@
+import json
 import scrapy
-from urllib.parse import urljoin
+import urllib.parse
+
+# from urllib.parse import urljoin
+import re
+import psycopg2
+
+# from bookscraper.bookscraper.items import AmazonItem
+
+from bookscraper.items import AmazonItem
 
 
-class AmazonSearchSpider(scrapy.Spider):
-    name = "amazon_search"
+class AmazonSearchProductSpider(scrapy.Spider):
+    name = "price_spider"
 
     custom_settings = {
         "FEEDS": {
@@ -13,71 +22,97 @@ class AmazonSearchSpider(scrapy.Spider):
         }
     }
 
+    def __init__(self):
+        self.connection = psycopg2.connect(
+            host="localhost", user="tiffanygong", password="testpass", dbname="project4"
+        )
+
+        self.cur = self.connection.cursor()
+        # Execute the query
+        self.cur.execute(
+            "SELECT title, contributors, price, email FROM base_book JOIN base_notification ON base_book.id = base_notification.book_id"
+        )
+
+        # Fetch all rows from the result set
+        notifications = self.cur.fetchall()
+
+        # Close the cursor and the connection
+        self.cur.close()
+        self.connection.close()
+
+        # Return the fetched notifications
+        self.notifications = notifications
+
     def start_requests(self):
-        keyword_list = ["ipad"]
-        for keyword in keyword_list:
-            amazon_search_url = f"https://www.amazon.com/s?k={keyword}&page=1"
+        notifications = self.notifications
+        # keyword_list = ["ipad"]
+        for notification in notifications:
+            title = notification[0]
+            contributors = notification[1].split(",")
+            target_price = notification[2]
+            email = notification[3]
+            keyword = f"{title} by {contributors[0]} paperback"
+            parsed_keyword = urllib.parse.quote_plus(keyword)
+
+            amazon_search_url = f"https://www.amazon.com/s?k={parsed_keyword}"
+
+            # amazon_search_url = f"https://www.amazon.com/s?k={keyword}&page=1"
             yield scrapy.Request(
                 url=amazon_search_url,
-                callback=self.parse_search_results,
-                meta={"keyword": keyword, "page": 1},
+                callback=self.discover_product_urls,
+                meta={
+                    "keyword": keyword,
+                    "target_price": target_price,
+                    "email": email,
+                },
             )
 
-    def parse_search_results(self, response):
-        page = response.meta["page"]
-        keyword = response.meta["keyword"]
+    def discover_product_urls(self, response):
+        amazon_item = AmazonItem()
+        target_price = response.meta["target_price"]
 
-        ## Extract Overview Product Data
-        search_products = response.css(
+        ## Discover Product URLs
+        product = response.css(
             "div.s-result-item[data-component-type=s-search-result]"
-        )
-        for product in search_products:
-            relative_url = product.css("h2>a::attr(href)").get()
-            asin = (
-                relative_url.split("/")[3]
-                if len(relative_url.split("/")) >= 4
-                else None
+        )[1]
+
+        dollar = product.css("span.a-price-whole::text").get("")
+        cent = product.css("span.a-price-fraction::text").get("")
+
+        if dollar is not None and int(dollar) < target_price:
+            amazon_item["name"] = (
+                product.css("h2.a-size-mini span.a-size-medium::text").get("").strip()
             )
-            product_url = urljoin("https://www.amazon.com/", relative_url).split("?")[0]
-            yield {
-                "keyword": keyword,
-                "asin": asin,
-                "url": product_url,
-                "ad": True if "/slredirect/" in product_url else False,
-                "title": product.css("h2>a>span::text").get(),
-                "price": product.css(
-                    ".a-price[data-a-size=xl] .a-offscreen::text"
-                ).get(),
-                "real_price": product.css(
-                    ".a-price[data-a-size=b] .a-offscreen::text"
-                ).get(),
-                "rating": (
-                    product.css("span[aria-label~=stars]::attr(aria-label)").re(
-                        r"(\d+\.*\d*) out"
-                    )
-                    or [None]
-                )[0],
-                "rating_count": product.css(
-                    "span[aria-label~=stars] + span::attr(aria-label)"
-                ).get(),
-                "thumbnail_url": product.xpath(
-                    "//img[has-class('s-image')]/@src"
-                ).get(),
-            }
+            amazon_item["price"] = int(dollar) + float(cent) / 100
+            amazon_item["email"] = response.meta["email"]
+
+            yield amazon_item
+
+        # relative_url = product.css("h2>a::attr(href)").get()
+        # product_url = urllib.parse.urljoin(
+        #     "https://www.amazon.com/", relative_url
+        # ).split("?")[0]
+        # yield scrapy.Request(
+        #     url=product_url,
+        #     callback=self.parse_product_data,
+        #     meta={"keyword": keyword, "page": page},
+        # )
 
         ## Get All Pages
-        if page == 1:
-            available_pages = response.xpath(
-                '//*[contains(@class, "s-pagination-item")][not(has-class("s-pagination-separator"))]/text()'
-            ).getall()
 
-            last_page = available_pages[-1]
-            for page_num in range(2, int(last_page)):
-                amazon_search_url = (
-                    f"https://www.amazon.com/s?k={keyword}&page={page_num}"
-                )
-                yield scrapy.Request(
-                    url=amazon_search_url,
-                    callback=self.parse_search_results,
-                    meta={"keyword": keyword, "page": page_num},
-                )
+    # def parse_product_data(self, response):
+
+    # price = response.css('.a-price-whole span[aria-hidden="true"] ::text').get("")
+    # price = response.css(".a-price .a-offscreen::text").get()
+    # if not price:
+    # price = response.css(".a-price .a-offscreen ::text").get("")
+    # if price is not None and price < target_price:
+
+    #     yield {
+    #         "name": response.css("#productTitle::text").get("").strip(),
+    #         "price": price,
+    #     }
+    # yield {
+    #     "name": response.css("#productTitle::text").get("").strip(),
+    #     "price": price,
+    # }
