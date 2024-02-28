590ef074e73c457eb881a386d193c82858876923
Tiffany Gong <gongt108@gmail.com>
2024-02-27 21:14:46 -0800

    change allowed_hosts

--- bookclub/settings.py
+++ bookclub/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # using gmail
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
